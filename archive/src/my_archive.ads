eeb8b71d2954ed957d0e1a929e3b927ec2af74ac
wair <washington.irving.e@gmail.com>
2015-04-29 17:08:23 +0200

    Delete my_archive.ads

--- archive/src/my_archive.ads
+++
@@ -1,8 +0,0 @@
------------------------------------------
--- Sorting package
------------------------------------------
-package My_Archive is
-   type Float_Vector is array (Positive range <>) of Float;
-   -- procedure for bubble sort
-   function Archive_it(A: in out Float_Vector) return Float_Vector;
-end My_Archive;
