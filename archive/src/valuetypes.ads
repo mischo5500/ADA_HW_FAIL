a0db86125da123ef440a9f1b9218e6cf31a67ea9
wair <martin.bednar@hotmail.com>
2015-05-06 10:04:53 +0200

    potrebujem value types file tak som ho pridal

---
+++ archive/src/valuetypes.ads
@@ -0,0 +1,49 @@
+with Ada.Real_Time; use Ada.Real_Time;
+with Ada.Strings.Bounded;
+
+package ValueTypes is
+
+  type TValueStatus_Elm is (Valid, Unknown);
+  type TValueStatus is array (TValueStatus_Elm'Range) of Boolean;
+  for TValueStatus'Component_Size use 1;
+
+  -- Hodnota
+  type TValue is record
+    --
+    value : Long_Float;
+    -- hodnota
+
+    timeStamp : Time;
+    -- casova znacka
+
+    status : TValueStatus;
+    -- priznak platnosti
+  end record;
+  --
+  unknownValue : constant TValue :=
+     (value => 0.0,
+      timeStamp => Time_First,
+      status => (Valid => False, Unknown => True)
+     );
+  validValue : constant TValue :=
+     (value => 0.0,
+      timeStamp => Time_First,
+      status => (Valid => True, Unknown => False)
+     );
+  --
+  VALUE_NAME_LENGTH : constant := 32;
+  --
+  package ValueName_Pkg is new Ada.Strings.Bounded.Generic_Bounded_Length (
+                                                                           VALUE_NAME_LENGTH);
+  use ValueName_Pkg;
+  -- meno hodnoty
+  subtype TValueName is ValueName_Pkg.Bounded_String;
+
+  CLIENT_NAME_LENGTH : constant := 32;
+  package ClientName_Pkg is new Ada.Strings.Bounded.Generic_Bounded_Length (
+                                                                            CLIENT_NAME_LENGTH);
+  use ClientName_Pkg;
+  -- meno hodnoty
+  subtype TClientName is ClientName_Pkg.Bounded_String;
+
+end ValueTypes;
