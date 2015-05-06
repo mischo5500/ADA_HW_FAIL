9580395c54e23abfe927c05c8f98ba6980088fd3
wair <martin.bednar@hotmail.com>
2015-05-06 12:38:11 +0200

    Timestamp is working FUCKgit add common/msgs/valuetypes.ads

--- archive/src/main.adb
+++ archive/src/main.adb
@@ -2,13 +2,19 @@ with My_Archive; use My_Archive;
 with Ada.Text_IO, Ada.Float_Text_IO; use Ada.Text_IO, Ada.Float_Text_IO;
 with Ada.Real_Time; use Ada.Real_Time;
 with ValueTypes; use ValueTypes;
+with Ada.Calendar;             use Ada.Calendar;
+with Ada.Calendar.Formatting;  use Ada.Calendar.Formatting;
+with Ada.Calendar.Time_Zones;  use Ada.Calendar.Time_Zones;
+with Ada.Text_IO;              use Ada.Text_IO;
+
 procedure Main is
 test : TValue :=
      (value => 0.7,
-      timeStamp => Clock,
+      timeStamp => Ada.Calendar.Clock,
       status => (Valid => False, Unknown => True)
      );
 begin
    My_Archive.Archive_it(test);
    Ada.Text_IO.Put_Line("Hello");
 end Main;
+--comment
